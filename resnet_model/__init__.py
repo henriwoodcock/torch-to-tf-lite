4823e0b62970b1890bbce41233c4a4c938a267a6
Henri Woodcock <henri.woodcock@arm.com>
2020-12-22 18:41:47 +0000

    added torch to onnx converter

--- resnet_model/__init__.py
+++ resnet_model/__init__.py
@@ -1,8 +1,70 @@
 import torch
 import torchvision
+import onnx
+import onnxruntime
 
-def load_model():
-  return torchvision.models.resnet18(pretrained=True)
+def load_model(load, model_path):
 
-def convert_torch_to_onnx():
+  if load:
+    model = torchvision.models.resnet18(pretrained=True)
+    torch.save(model, model_path / 'resnet.pth')
+
+  else:
+    model = torch.load(model_path / 'resnet.pth')
+
+  return model
+
+def create_rand_tens():
+  return torch.randn(1, 3, 224, 224, requires_grad=True)
+
+def to_numpy(tensor):
+  return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+def convert_torch_to_onnx(model, onnx_path):
+  '''convert a torch model in an onnx model
+
+  args:
+    - model: torch.Module (torch model)
+    - onnx_path: pathlib.Path
+  '''
+  #generate random tensor to use
+  rand_tens = create_rand_tens()
+  #get torch model output
+  torch_out = model(rand_tens)
+  torch.onnx.export(
+    model=model,
+    args=rand_tens,
+    f=onnx_path / 'resnet.onnx',
+    verbose=False,
+    export_params=True,
+    do_constant_folding=False,
+    input_names=['input'],
+    output_names=['output'],
+    dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
+                  'output' : {0 : 'batch_size'}})
+  )
+
+  onnx_model = onnx.load(onnx_path / 'resnet.onnx')
+  onnx.checker.check_model(onnx_model)
+
+
+  ort_session = onnxruntime.InferenceSession(onnx_path / 'resnet.onnx')
+  ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+  ort_outs = ort_session.run(None, ort_inputs)
+  # compare ONNX Runtime and PyTorch results
+  np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03,
+                              atol=1e-05)
+  print("Exported model has been tested with ONNXRuntime, and the result looks"\
+        " good!")
+
+
   return None
+
+
+
+
+
+
+
+
+
--- resnet_model/__main__.py
+++ resnet_model/__main__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 
 import resnet_model
@@ -5,8 +7,13 @@ import resnet_model
 @click.command()
 @click.option('--load', '-L', is_flag=True)
 def main(load):
+  modelLoc = Path('models')
+  #load model from dl or locally
+  model = resnet_model.load_model(load, modelLoc)
+  #put in evaluate mode
+  model.eval()
 
-  if load: model = resnet_model.load_model()
+  if convert_torch: resnet_model.convert_torch_to_onnx(model, modelLoc)
 
   return None
 
