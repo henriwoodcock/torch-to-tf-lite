9632f698acfe664b8cb290c9d3c0f18414666946
Henri Woodcock <henri.woodcock@arm.com>
2020-12-30 11:12:36 +0000

    added convert onnx to keras to main

--- torch_to_tf_lite/__main__.py
+++ torch_to_tf_lite/__main__.py
@@ -13,12 +13,14 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 @click.option('--convert_torch', '-T', is_flag=True)
 @click.option('--convert_onnx', '-O', is_flag=True)
 @click.option('--convert_tflite', '-mu', is_flag = True)
+@click.option('--convert_keras', 'K', is_flag=True)
 @click.option('--prune_weights', '-PW', is_flag = True)
-def main(load, convert_torch, convert_onnx, convert_tflite, prune_weights):
+def main(load, convert_torch, convert_onnx, convert_tflite, convert_keras,
+        prune_weights):
   modelLoc = Path('models')
   dataLoc = Path('data')
   #load model from dl or locally
-  model, acc = resnet_model.load_model(load, modelLoc, dataLoc)
+  model, acc = torch_to_tf_lite.load_model(load, modelLoc, dataLoc)
   print('model accuracy = ', acc)
   logging.info('accuracy on baseline model')
   logging.info(accuracy.numpy())
@@ -26,17 +28,21 @@ def main(load, convert_torch, convert_onnx, convert_tflite, prune_weights):
   #put in evaluate mode
   model.eval()
   # create an onnx model
-  if convert_torch: resnet_model.convert_torch_to_onnx(model, modelLoc)
+  if convert_torch: torch_to_tf_lite.convert_torch_to_onnx(model, modelLoc)
   # convert onnx to tf frozen graph
   if convert_onnx:
-    resnet_model.convert_onnx_to_tf(modelLoc / 'resnet.onnx',
+    torch_to_tf_lite.convert_onnx_to_tf(modelLoc / 'resnet.onnx',
                                     modelLoc / 'resnet.pb')
 
   if convert_tflite:
-    resnet_model.convert_frozen_graph_to_tflite(modelLoc)
+    torch_to_tf_lite.convert_frozen_graph_to_tflite(modelLoc)
+
+  if convert_keras:
+    torch_to_tf_lite.convert_onnx_to_keras(modelLoc / 'resnet.onnx',
+                                           modelLoc / 'keras')
 
   if prune_weights:
-    acc = resnet_model.prune_torch_weights(model, modelLoc, dataLoc, 0.25)
+    acc = torch_to_tf_lite.prune_torch_weights(model, modelLoc, dataLoc, 0.25)
     print('accuracy after pruning = ', acc.numpy())
     logging.info('accuracy after pruning:')
     logging.info(acc.numpy())
